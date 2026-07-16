5e6a69282ca7796ff3f6a6635d2d69ed4cfee887
richi78 <ricardo.rojas.carvajal@gmail.com>
2026-07-16 12:29:06 -0400

    fix: update link text from 'Manuals' to 'Docs' in PortfolioProjects component

--- frontend/app/features/portfolio/components/PortfolioProjects.vue
+++ frontend/app/features/portfolio/components/PortfolioProjects.vue
@@ -85,7 +85,7 @@ const { projects } = usePortfolio()
                   <line x1="16" y1="17" x2="8" y2="17" />
                   <polyline points="10 9 9 9 8 9" />
                 </svg>
-                Manuals
+                Docs
               </a>
             </div>
           </div>
